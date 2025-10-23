94d55723f2cdd14e28dcf83722d3a6b84636e3fb
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-10-23 10:53:01 +0600

    first commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import AuthForm from "@/components/AuthForm";
+
+const SignIn = () => {
+  return (
+    <div className="my-20">
+      <AuthForm type="sign-in" />;
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import AuthForm from "@/components/AuthForm";
+
+const SignUp = () => {
+  return (
+    <div className="my-20">
+      <AuthForm type="sign-up" />
+    </div>
+  );
+};
+
+export default SignUp;
