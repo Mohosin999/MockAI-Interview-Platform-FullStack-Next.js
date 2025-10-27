3e8d68e393966ff21d3b0950a8f9f6a98b83d485
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-10-27 21:38:57 +0600

    try to call from home page

--- app/api/home-feedback/route.ts
+++ app/api/home-feedback/route.ts
@@ -37,11 +37,12 @@ export async function POST(req: NextRequest) {
     const prompt = `
     You are an AI classifier. Based on the conversation transcript,
     decide whether it is a "mock-interview" or "interview-questions".
-    Return ONLY one of these two words, nothing else. If user is not response within the transcript, return empty string.
-
-      Transcript:
-      ${formattedTranscript}
+    Return ONLY one of these two words, nothing else.
+    
+    Transcript:
+    ${formattedTranscript}
     `.trim();
+    // If user is not response within the transcript, return empty string.
 
     // Initialize Gemini model
     const ai = new GoogleGenerativeAI(apiKey);
